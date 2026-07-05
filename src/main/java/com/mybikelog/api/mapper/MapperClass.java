5e2c861fd878c70e37ce6fcf0932eac792c06f3c
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2026-07-05 21:55:38 +0530

    Implemented map-struct and Database related code

---
+++ src/main/java/com/mybikelog/api/mapper/MapperClass.java
@@ -0,0 +1,12 @@
+package com.mybikelog.api.mapper;
+
+import com.mybikelog.api.dto.UsersDTO;
+import com.mybikelog.api.entity.Users;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MapperClass {
+    UsersDTO toUsersDto(Users user);
+
+    Users toEntity(UsersDTO dto);
+}
