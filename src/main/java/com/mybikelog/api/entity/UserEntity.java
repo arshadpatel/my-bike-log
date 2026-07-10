80799e503e2d66f5013e74a3984915d6801befff
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2026-07-10 23:37:20 +0530

    Implemented users/me endpoint completed Users Controller Changes

--- src/main/java/com/mybikelog/api/entity/UserEntity.java
+++ src/main/java/com/mybikelog/api/entity/UserEntity.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Data
 @Entity
-public class Users {
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
